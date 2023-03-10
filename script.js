279323da601ba83200069cea18e37ee5dac72644
unknown <joan.tomas.miralles@gmail.com>
2023-03-10 09:45:43 +0100

    Añadido boton para copar texto a portapapeles

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-
-
 /* --------------------------------------- VARIABLES --------------------------------------- */
 
 const texto = document.getElementById("texto");
 const botonBorrar = document.getElementById("boton-borrar");
+const botonCopiar = document.getElementById("boton-copiar");
 const resultado = document.getElementById("resultado");
 const numPalabras = document.getElementById("num-palabras");
 const numEspacios = document.getElementById("num-espacios");
@@ -11,6 +10,9 @@ const numCaracteres = document.getElementById("num-caracteres");
 const numAcentos = document.getElementById("num-acentos");
 const numParrafos = document.getElementById("num-parrafos");
 const tiempoLectura = document.getElementById("tiempo-lectura");
+const TextoCompleto = document.getElementById("texto");
+const palabrasRepetidas = document.getElementById("palabras-repetidas");
+
 
 
 /* --------------------------------------- CONTAR PALABRAS --------------------------------------- */
@@ -43,3 +45,42 @@ botonBorrar.addEventListener("click", () => {
   texto.value = "";
   actualizarContadores();
 });
+
+//---------------------------------- Copiar el contenido del textarea -------------------------------------
+
+function copyToClipboard(TextoCompleto) {
+  TextoCompleto.select();
+  document.execCommand("copy");
+}
+
+botonCopiar.addEventListener("click", () => {
+  copyToClipboard(TextoCompleto);
+});
+
+//---------------------------------- Boton del modal texto copiado  -------------------------------------
+
+// Get the modal element
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("boton-copiar");
+
+// Get the close button element
+var closeBtn = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on the close button, close the modal
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
